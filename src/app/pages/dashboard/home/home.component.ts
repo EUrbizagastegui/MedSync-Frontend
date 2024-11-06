b503ce260e9c1ff65fd1a760696dfb3058d878a0
EUrbizagastegui <erickurbizagastegui@hotmail.com>
2024-11-05 21:11:05 -0500

    bugfix

--- src/app/pages/dashboard/home/home.component.ts
+++ src/app/pages/dashboard/home/home.component.ts
@@ -1,30 +1,12 @@
 import { Component } from '@angular/core';
-import { NgCircleProgressModule, CircleProgressOptions } from 'ng-circle-progress';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NgCircleProgressModule],
-  providers: [
-    {
-      provide: CircleProgressOptions,
-      useValue: {
-        backgroundPadding: -4,
-        radius: 50,
-        outerStrokeWidth: 10,
-        outerStrokeColor: "#06b6d4",
-        innerStrokeWidth: 4,
-        innerStrokeColor: "#0e7490",
-        animation: true,
-        animationDuration: 300,
-        showBackground: false,
-        showUnits: false,
-        showSubtitle: false
-      }
-    }
-  ],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  progress = 75;
 }
