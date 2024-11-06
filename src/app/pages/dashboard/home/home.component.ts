021fe524ab3e76f99ef4721f04dd74ba4944fee8
EUrbizagastegui <erickurbizagastegui@hotmail.com>
2024-11-06 00:26:12 -0500

    Agregado nuevo circular progressbar.

--- src/app/pages/dashboard/home/home.component.ts
+++ src/app/pages/dashboard/home/home.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+// https://www.npmjs.com/package/angular-svg-round-progressbar
+import {RoundProgressComponent} from 'angular-svg-round-progressbar';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RoundProgressComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
   progress = 75;
+  max=100;
 }
