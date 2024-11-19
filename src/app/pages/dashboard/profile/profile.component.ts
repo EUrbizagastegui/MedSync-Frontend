f2cae29b7df4cbed0161d7b9b426042cd9646e37
Alonso Robles <u202112662@upc.edu.pe>
2024-11-19 08:26:30 -0500

    Actualización de perfil

--- src/app/pages/dashboard/profile/profile.component.ts
+++ src/app/pages/dashboard/profile/profile.component.ts
@@ -9,6 +9,8 @@ import {ToastModule} from 'primeng/toast';
 import {ImageModule} from 'primeng/image';
 import {PatientService} from '../../../services/patient/patient.service';
 import {MessageService} from 'primeng/api';
+import {CarerService} from '../../../services/carer/carer.service';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-profile',
@@ -21,26 +23,39 @@ import {MessageService} from 'primeng/api';
     InputGroupModule,
     InputTextModule,
     ToastModule,
-    ImageModule
+    ImageModule,
+    NgIf
   ],
   providers: [MessageService],
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css'] // Corrección: `styleUrls` en lugar de `styleUrl`
+  styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
   displayDialog: boolean = false;
   displayDialog1: boolean = false;
+  displayDialog2: boolean = false;
   userId: number = +localStorage.getItem('userId')!;
+  userRole: string = localStorage.getItem('userRole')!;
+
+  isPatient: boolean = false;
+  isCarer: boolean = false;
 
   constructor(
     private patientService: PatientService,
-    private messageService: MessageService
+    private messageService: MessageService,
+    private carerService: CarerService,
   ) {}
 
   ngOnInit() {
-    console.log(this.userId);
-    this.loadPatientData();
-    this.loadCarerData(); // Nueva llamada para cargar la información del cuidador
+    this.isPatient = this.userRole === 'PATIENT';
+    this.isCarer = this.userRole === 'CARER';
+
+    if (this.isPatient) {
+      this.loadPatientData();
+      this.loadCarerData();
+    } else if (this.isCarer) {
+      this.loadCarerProfile();
+    }
   }
 
   information = {
@@ -89,7 +104,7 @@ export class ProfileComponent implements OnInit {
   loadCarerData() {
     this.patientService.getPatientCarer(this.userId.toString()).subscribe({
       next: (data) => {
-        console.log('Datos del cuidador:', data); // Verifica qué datos llegan
+        console.log('Datos del cuidador:', data);
         this.contactData = { ...this.contactData, ...data };
       },
       error: (err) => {
@@ -98,7 +113,23 @@ export class ProfileComponent implements OnInit {
           summary: 'Advertencia',
           detail: 'El paciente no tiene un cuidador asignado'
         });
-        console.error('Error al cargar datos del cuidador:', err); // Muestra el error en consola
+        console.error('Error al cargar datos del cuidador:', err);
+      }
+    });
+  }
+
+  loadCarerProfile() {
+    this.carerService.getCarerById(this.userId).subscribe({
+      next: (data) => {
+        this.contactData = { ...this.contactData, ...data };
+      },
+      error: (err) => {
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Error',
+          detail: 'No se pudo cargar la información del cuidador'
+        });
+        console.error(err);
       }
     });
   }
@@ -109,39 +140,73 @@ export class ProfileComponent implements OnInit {
   }
 
   saveEdit() {
-    const updatedProfile = {
-      disease: this.information.disease,
-      weight: this.information.weight,
-      profilePictureUrl: this.information.profilePictureUrl,
-      phoneNumber: this.information.phoneNumber
-    };
+    if (this.isPatient) {
+      const updatedProfile = {
+        disease: this.information.disease,
+        weight: this.information.weight,
+        profilePictureUrl: this.information.profilePictureUrl,
+        phoneNumber: this.information.phoneNumber
+      };
 
-    this.patientService.updatePatient(this.userId.toString(), updatedProfile).subscribe({
-      next: () => {
-        this.messageService.add({
-          severity: 'success',
-          summary: 'Éxito',
-          detail: 'Perfil actualizado correctamente'
-        });
-        this.displayDialog = false;
-      },
-      error: (err) => {
-        this.messageService.add({
-          severity: 'error',
-          summary: 'Error',
-          detail: 'No se pudo actualizar el perfil'
-        });
-        console.error(err);
+      this.patientService.updatePatient(this.userId.toString(), updatedProfile).subscribe({
+        next: () => {
+          this.messageService.add({
+            severity: 'success',
+            summary: 'Éxito',
+            detail: 'Perfil actualizado correctamente'
+          });
+          this.displayDialog = false;
+        },
+        error: (err) => {
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail: 'No se pudo actualizar el perfil'
+          });
+          console.error(err);
+        }
+      });
+    } else if (this.isCarer) {
+      const updatedProfileCarer = {
+        phoneNumber: this.contactData.phoneNumber,
+        profilePictureUrl: this.contactData.profilePictureUrl
       }
-    });
+
+      this.carerService.updateCarer(this.userId, updatedProfileCarer).subscribe({
+        next: () => {
+          this.messageService.add({
+            severity: 'success',
+            summary: 'Éxito',
+            detail: 'Perfil actualizado correctamente'
+          });
+          this.displayDialog2 = false;
+        },
+        error: (err) => {
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail: 'No se pudo actualizar el perfil'
+          });
+          console.error(err);
+        }
+      });
+    }
   }
 
   cancelEdit() {
     this.displayDialog = false;
   }
 
+  cancelEditCarer() {
+    this.displayDialog2 = false;
+  }
+
   openDialogContact() {
     this.displayDialog1 = true;
+  }
+
+  openDialogContact2() {
+    this.displayDialog2 = true;
   }
 
   saveContact() {
